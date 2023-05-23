0784f1b2b34dccf686265cada9dffd9d1c12a331
oOHiyoriOo <unconfigured@null.spigotmc.org>
2023-05-23 14:17:00 +0200

    minimal conf

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ rm -rf /home/container/tmp/*
 
 echo "⟳ Starting Nginx..."
 echo "✓ Successfully started"
-/usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/
+/usr/local/nginx/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/
